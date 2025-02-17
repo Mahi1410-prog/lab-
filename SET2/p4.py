a97d1656dfe30d8ed24c3527473823d9e9eb4dd3
Mahi1410-prog <mahirana7616@gmail.com>
2025-02-17 08:43:59 +0530

    add p1.py fahrenheit and celsius add p2.py add p3.py add p4.py add p5.py add p6.py add p7.py add p8.py add p9.py add p10.py add p1.py add p2.py add p3.py add p4.py add p1 app.py index.html add p4.py,file csv

--- SET2/p4.py
+++ SET2/p4.py
@@ -2,7 +2,7 @@ import csv
 
 def calculate_average(csv_file, column_name):
     try:
-        with open(csv_file, 'r') as file:
+         with open(csv_file, 'r') as file:
             reader = csv.DictReader(file)
             if column_name not in reader.fieldnames:
                 print(f"Column '{column_name}' not found.")
@@ -21,9 +21,8 @@ def calculate_average(csv_file, column_name):
     except FileNotFoundError:
         print(f"File '{csv_file}' not found.")
         return None
-
-csv_file_path = 'file.csv'
-column_to_calculate = 'ENGLISH'
+csv_file_path = 'src/file.csv'
+column_to_calculate = ' Total Marks'
 
 result = calculate_average(csv_file_path, column_to_calculate)
 if result is not None:
